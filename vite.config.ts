27483a7812f726deb7d7a8ea0b962a95fb042b37
Oliver Lee <ol558@nyu.edu>
2024-05-05 15:16:45 -0400

    base name change

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goya_trauma_website",
+  base: "/Goya-Trauma_website",
   plugins: [react()],
 });
